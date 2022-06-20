a38481b76df8ce490a204a05e31b4768d4ae2a71
Derrick <derrickryan10@gmail.com>
2022-06-20 14:38:53 -0400

    Add lists working

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// const items = document.querySelector('#items');
+const buttons = document.querySelector('.btns');
+
 const addItemBtn = document.querySelector('.add-item');
 const addInput = document.querySelector('#add-input');
 const addInputBtn = document.querySelector('#add-input-btn');
 const itemText = document.querySelector('#item-text');
-const items = document.querySelector('.items');
+
+const addListBtn = document.getElementById('list-add');
+const listInput = document.getElementById('list-input');
+const listInputBtn = document.getElementById('list-btn')
+const listText = document.getElementById('list-text');
 
 addItemBtn.addEventListener('click', showAddItem);
 addInputBtn.addEventListener('click', addItem);
+
+addListBtn.addEventListener('click', showAddList);
+listInputBtn.addEventListener('click', addList);
+
+document.getElementById('To Do').addEventListener('click', openList);
 
 function showAddItem() {
     itemText.value = '';
@@ -18,7 +30,20 @@ function hideAddItem() {
     addItemBtn.style.display = 'flex';
 }
 
+function showAddList() {
+    listText.value = '';
+    addListBtn.style.display = 'none';
+    listInput.style.display = 'flex';
+}
+
+function hideAddList(){
+    listInput.style.display = 'none';
+    addListBtn.style.display = 'flex';
+}
+
 function addItem(){
+    const items = document.querySelector(".current");
+
     const newItem = document.createElement('div');
     newItem.classList.add('item');
 
@@ -56,4 +81,40 @@ function circleClose(e) {
 
 function xClose(e) {
     e.target.parentNode.parentNode.remove();
+}
+
+function addList(){
+    const newBtn = document.createElement('button');
+    newBtn.classList.add('nav-btn');
+    newBtn.id = listText.value;
+    newBtn.textContent = listText.value;
+    newBtn.addEventListener('click', openList);
+    buttons.appendChild(newBtn);
+    buttons.appendChild(document.createElement('br'));
+    
+    const newDiv = document.createElement('div');
+    newDiv.id = listText.value +"div";
+    document.getElementById('all-items').appendChild(newDiv);
+
+    hideAddList();
+}
+
+function openList(e) {
+    const newId = e.target.id;
+
+    const currentButton = document.querySelector('.active');
+    currentButton.classList.remove('active');
+
+    const nextButton = document.getElementById(newId);
+    nextButton.classList.add('active');
+
+    document.getElementById('current-head').textContent = newId;
+
+    const currentList = document.querySelector('.current');
+    currentList.classList.remove('current');
+    currentList.style.display = 'none';
+
+    const nextList = document.getElementById(newId +"div");
+    nextList.classList.add('current');
+    nextList.style.display = 'block';
 }
